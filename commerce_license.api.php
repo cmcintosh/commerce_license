644082d5108a36871bb82c69e5a8424d7a527639
Bojan Zivanovic <bojanz@gmail.com>
2013-07-17 10:43:11 +0200

    Add a hook_commerce_license_types_list_alter().

---
+++ commerce_license.api.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the Commerce License module.
+ */
+
+/**
+ * Alter the list of available license types shown on the product form.
+ *
+ * Allows modules to make their license types available only to certain
+ * licensable product types.
+ *
+ * @param $types
+ *   List of license types.
+ * @param $product
+ *   The parent product.
+ */
+function hook_commerce_license_types_list_alter(&$types, $product) {
+  $product_types = variable_get('commerce_file_product_types', array());
+  $product_types = array_filter($product_types);
+  if (!in_array($product->type, $product_types)) {
+    unset($types['file']);
+  }
+}
