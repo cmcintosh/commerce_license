03a94d4726d061c2e0ba30445c1b9000106ed990
andyg5000 <andyg5000@808668.no-reply.drupal.org>
2014-01-16 16:47:49 +0100

    Issue #2166015 by andyg5000: commerce_license_role causes entity metadata property errors on add to cart submit.

--- modules/commerce_license_role/plugins/license_type/CommerceLicenseRole.class.php
+++ modules/commerce_license_role/plugins/license_type/CommerceLicenseRole.class.php
@@ -18,14 +18,14 @@ class CommerceLicenseRole extends CommerceLicenseBase  {
    * Maintains the role, adding or removing it from the owner when necessary.
    */
   public function save() {
-    if ($this->uid) {
+    if ($this->uid && $this->product_id) {
       $role = $this->wrapper->product->commerce_license_role->value();
       $owner = $this->wrapper->owner->value();
       $save_owner = FALSE;
       if (!empty($this->license_id)) {
         $this->original = entity_load_unchanged('commerce_license', $this->license_id);
         // A plan change occurred. Remove the previous role.
-        if ($this->product_id != $this->original->product_id) {
+        if ($this->original->product_id && $this->product_id != $this->original->product_id) {
           $previous_role = $this->original->wrapper->product->commerce_license_role->value();
           if (isset($owner->roles[$previous_role])) {
             unset($owner->roles[$previous_role]);
